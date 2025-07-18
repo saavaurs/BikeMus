04438f2313f8bacf5e4e294931d6f6c61662aca8
zahraaddina <addinazahra14@gmail.com>
2025-07-18 22:40:25 +0700

    navbar sudah okeh insyaallah ye

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
-import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
+import { Image } from 'react-native';
 
 export default function TabsLayout() {
   return (
@@ -22,13 +22,19 @@ export default function TabsLayout() {
         headerShown: false,
       }}
     >
-
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('../../assets/images/home.png')}
+              style={{
+                width: 24,
+                height: 24,
+                tintColor: focused ? '#8B1C3F' : '#666',
+              }}
+            />
           ),
         }}
       />
@@ -36,8 +42,15 @@ export default function TabsLayout() {
         name="scanqr"
         options={{
           title: 'Scan QR',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="list" size={size} color={color} />
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('../../assets/images/qr.png')}
+              style={{
+                width: 24,
+                height: 24,
+                tintColor: focused ? '#8B1C3F' : '#666',
+              }}
+            />
           ),
         }}
       />
@@ -45,8 +58,15 @@ export default function TabsLayout() {
         name="riwayat"
         options={{
           title: 'Riwayat',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="list" size={size} color={color} />
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('../../assets/images/riwayat.png')}
+              style={{
+                width: 24,
+                height: 24,
+                tintColor: focused ? '#8B1C3F' : '#666',
+              }}
+            />
           ),
         }}
       />
